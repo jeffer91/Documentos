eed7608b509946b92af840b5f1ffaa292acce409
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-28 13:28:17 -0500

    Release: interfaz 2.8.1

--- preload.cjs
+++ preload.cjs
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("documentosApp", {
-  version: "2.8.0",
+  version: "2.8.1",
   getCatalog: () => ipcRenderer.invoke("catalog:get"),
   createProject: (meta) => ipcRenderer.invoke("projects:create", meta),
   listProjects: () => ipcRenderer.invoke("projects:list"),
